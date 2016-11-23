3f4db9e91c33dae5ca0e273159f4e9fd603291a9
Srar <a1020938070@gmail.com>
2016-11-24 02:34:10 +0800

    docnav添加url支持

--- src/components/DocsNav.js
+++ src/components/DocsNav.js
@@ -9,8 +9,20 @@ class DocsNav extends Component {
 		};
 	}
 
+	isUrl(path) {
+		if(path.length < 6) {
+			return false;
+		}
+
+		if(path.substring(0, 7) == "http://" || path.substring(0, 8) == "https://") {
+			return true;
+		}
+
+		return false;
+	}
 
 	render() {
+		var _this = this;
 		return (
 			<section className="docs-sidebar-left">
 				<nav className="docs-nav">
@@ -21,12 +33,17 @@ class DocsNav extends Component {
 								<h1><a href="#">{item.title}</a></h1>
 								<ul className="expanded">
 									{item.childs.map((child, childIndex) => {
+										var display = child.display == undefined ? true : false;
+										if(!display) {
+											return null;
+										}
+
 										return (
 											<li key={`docs-nav-item-${item.title}-${child.title}`} 
 											    style={child.style ? child.style : {}}
 												className={child.className ? child.className : ""}>
-												{/*<Link to={`${this.props.root || ""}${itemIndex}-${childIndex}`}>{child.title}</Link>*/}
-												<a href={`${this.props.root || ""}${itemIndex}-${childIndex}`}>{child.title}</a>
+												{/*<Link to={`${this.props.root || ""}${itemIndex}-${childIndex}`}>{child.title}</Link>*/}			
+												<a href={_this.isUrl(child.path) ? child.path : `${this.props.root || ""}${itemIndex}-${childIndex}`}>{child.title}</a>
 											</li>
 										)
 									})}
